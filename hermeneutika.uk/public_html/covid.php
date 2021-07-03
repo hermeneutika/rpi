d60941f3970745bc456fee6bdf3cbdd698684ed2
hermeneutika <hermeneutika@msn.com>
2021-07-03 20:30:19 +0100

    3/7/21 2019

---
+++ hermeneutika.uk/public_html/covid.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html >
+
+<html>
+<head>
+<title>Page title</title>
+<link href="site.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<?php include ("menu.php"); ?>
+
+
+
+<p>This is about Covid </p>
+<p>The reaction of one person</p>
+
+<video src="video/1.mp4" width="500" height="250" controls preload="false" ></video>
+<video src="video/3-1.mp4" width="500" height="250" controls preload="false"></video>
+
+
+</body>
+</html>
+
---
+++ hermeneutika.uk/public_html/index.php
@@ -0,0 +1,44 @@
+<<<<<<< HEAD
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+
+<html>
+<head>
+<title>Page title</title>
+<link href="site.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<?php include ("menu.php"); ?>
+
+
+
+<p>This site is dedicated to the Supreme Being and Architect of the Cosmos. </p>
+<p>The entire Christian Godhead</p>
+
+
+
+</body>
+</html>
+=======
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1" /> 
+<meta http-equiv="Cache-Control" content="no-store" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="menu1.css" rel="stylesheet" type="text/css" />
+<title>Hermeneutika's home pages</title>
+</head>
+<body>
+
+<?php include ("link.php") ?>
+
+<?php include ("menu.php"); ?>
+
+
+
+<p>This site is dedicated to the Supreme Being and Architect of the Cosmos. </p>
+<p>The entire Christian Godhead</p>
+</body>
+</html>
+>>>>>>> dcd2484400907c00bc113b0afb3977c34a07b210
